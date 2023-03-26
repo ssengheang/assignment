20209c8254df692873f5f851cd332317066da52d
Seng heang <sengzin097@gmail.com>
2023-03-26 14:48:42 +0700

    draft CRUD api

--- src/main/java/com/example/groupassessment/controller/RepaymentController.java
+++ src/main/java/com/example/groupassessment/controller/RepaymentController.java
@@ -1,5 +1,54 @@
 package com.example.groupassessment.controller;
 
-public class RepaymentController {
+import com.example.groupassessment.enitity.Repayment;
+import com.example.groupassessment.repository.RepaymentRepo;
+import com.example.groupassessment.services.RepaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResourceAccessException;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/repayments")
+public class RepaymentController {
+    private RepaymentRepo repaymentRepo;
+    @Autowired
+    private RepaymentController(RepaymentRepo repaymentRepo){
+        this.repaymentRepo = repaymentRepo;
+    }
+
+    @GetMapping("")
+    public List<Repayment> getAll(){
+        return repaymentRepo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Repayment> getOne(@PathVariable(value = "id") Long id){
+        return repaymentRepo.findById(id);
+    }
+
+    @PostMapping("")
+    public Repayment create(@Validated @RequestBody Repayment repayment_params){
+        return repaymentRepo.save(repayment_params);
+    }
+
+    @PutMapping("/{id}")
+    public Repayment update(@PathVariable(value = "id") Long id, @Validated @RequestBody Repayment repayment_params){
+        Repayment repayment = repaymentRepo.findById(id)
+                .orElseThrow(() -> new ResourceAccessException("Employee not found for this id ::" + id));;
+        repayment.setAmount(repayment_params.getAmount());
+        repayment.setDate(repayment_params.getDate());
+        repayment.setInterestDue(repayment_params.getInterestDue());
+        return repaymentRepo.save((repayment));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable(value = "id") Long id){
+        Repayment repayment = repaymentRepo.findById(id)
+                .orElseThrow(() -> new ResourceAccessException("Employee not found for this id ::" + id));;
+        repaymentRepo.delete(repayment);
+    }
 }
--- src/main/java/com/example/groupassessment/services/RepaymentService.java
+++ src/main/java/com/example/groupassessment/services/RepaymentService.java
@@ -1,10 +1,12 @@
 package com.example.groupassessment.services;
 
 import com.example.groupassessment.repository.RepaymentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class RepaymentService {
     private RepaymentRepo repaymentRepo;
 
+    @Autowired
     public RepaymentService(RepaymentRepo repaymentRepo){
         this.repaymentRepo = repaymentRepo;
     }
