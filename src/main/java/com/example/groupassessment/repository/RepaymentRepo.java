53802f2736bebfd0fb5454549ade52a7d8190607
Seng heang <sengzin097@gmail.com>
2023-03-26 13:56:34 +0700

    added repayment repo

---
+++ src/main/java/com/example/groupassessment/repository/RepaymentRepo.java
@@ -0,0 +1,8 @@
+package com.example.groupassessment.repository;
+
+import com.example.groupassessment.enitity.Repayment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepaymentRepo extends JpaRepository<Repayment, Long> {
+
+}
