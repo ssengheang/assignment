8e37e05db2d85757b43ab85c460273e2a1676ae6
Seng heang <sengzin097@gmail.com>
2023-04-06 19:34:50 +0700

    create loan repo

---
+++ src/main/java/com/example/groupassessment/repository/LoanRepo.java
@@ -0,0 +1,7 @@
+package com.example.groupassessment.repository;
+
+import com.example.groupassessment.enitity.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanRepo extends JpaRepository<Long, Loan> {
+}
